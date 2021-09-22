f6e978253d78de8fba25b4f8cfad2c47faf80d69
kayodds <91171049+kayodds@users.noreply.github.com>
2021-09-22 01:43:28 -0300

    Update moons.js

--- moons.js
+++ moons.js
@@ -7,6 +7,12 @@ const moon_array=["Slipping Through the Poison Tide",
                   "Good Morning, Captain Toad!"
                   "Caveman Cave-Fan",
                   "Just a Hat, Skip, and a Jump",
-                  "Treasure Under the Cliff"
-                  "Guarded by a Colossal Fossil"];
+                  "Treasure Under the Cliff",
+                  "Guarded by a Colossal Fossil",
+                  "Across the Mysterious Clouds",
+                  "Showdown on the Inverted Pyramid",
+                  "The Hole in the Desert",
+                  "Alcove in the Ruins",
+                  "Secret of the Mural,
+                  "Where the Birds Gather"];
 output=moon_array[Math.floor(Math.random()*moon_array.length)]
