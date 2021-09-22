a9b7163bac4d8db350cc041481e723a234030484
kayodds <91171049+kayodds@users.noreply.github.com>
2021-09-22 00:28:25 -0300

    Update moons.js

--- moons.js
+++ moons.js
@@ -1,1 +1,5 @@
-fish
+const moon_array=["Slipping Through the Poison Tide",
+                  "The Forgotten Treasure",
+                  "Under the Big One's Brim",
+                  "Roll On and On"];
+output=moon_array[Math.floor(Math.random()*moon_array.length)]
