144e8cb76ef9c49f228aa2f7c9281a2e2a1a6ef2
kayodds <91171049+kayodds@users.noreply.github.com>
2021-09-23 14:11:15 -0300

    Create singles.js

---
+++ singles.js
@@ -0,0 +1,13 @@
+const singles_array=["Alex",
+                     "Elliott",
+                     "Harvey",
+                     "Sam",
+                     "Sebastian",
+                     "Shane",
+                     "Abigail",
+                     "Emily",
+                     "Haley",
+                     "Leah",
+                     "Maru",
+                     "Penny"];
+output=singles_array[Math.floor(Math.random()*singles_array.length)]
