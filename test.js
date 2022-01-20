0c32dfa5f3735b45f675c849e34d96395c8fa55f
kayodds <91171049+kayodds@users.noreply.github.com>
2022-01-20 20:51:22 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,3 @@
 const test_array=[["Test2 was", "Test2 do be"],
                   ["This test2 is","Test2 is indeed"]];
-const a = Math.floor(Math.random()*2);
-const b = Math.floor(Math.random()*2);
-output=test_array[a][b]+' succesful'
+output=test_array
