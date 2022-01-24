7b66d32cebc1630600b4fd04d50b6b969b8db033
kayodds <91171049+kayodds@users.noreply.github.com>
2022-01-24 16:33:34 -0300

    Create facts.js

---
+++ facts.js
@@ -0,0 +1,5 @@
+const facts_array=["To this day, there have been 27 versions of the US flag.",
+                  "There are 17 flags with birds: 8 of them are eagles.",
+                  "The only national flag that doesn't have blue, red or white, is the Jamaican one 🇯🇲 .",
+                  "Flags with dragons: Wales, Bhutan and Malta. You'll need to zoom in for the last one." ];
+output=facts_array[Math.floor(Math.random()*facts_array.length)]
