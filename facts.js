acb8aa76db5074c1465e9468b978170b28179ad1
kayodds <91171049+kayodds@users.noreply.github.com>
2022-01-26 11:53:15 -0300

    Update facts.js

--- facts.js
+++ facts.js
@@ -3,11 +3,12 @@ const facts_array=["To this day, there have been 27 versions of the US flag.",
                   "The only national flag that doesn't have blue, red or white, is the Jamaican one 🇯🇲 .",
                   "Flags with dragons: Wales, Bhutan and Malta. You'll need to zoom in for the last one.",
                   "Denmark 🇩🇰 holds the oldest national flag in us. It is said to have felt from the sky during a battle",
-                  "An old prophecy stated that an eagle on a cactus eating snake would signal the Mexica where to found their city. These symbols are still in the Mexican 🇲🇽 flag",
+                  "An old prophecy stated that an eagle on a cactus eating snake would signal the Mexica people where to found their city 🇲🇽.",
                   "Up until 2011 the Lybian flag was a solid green rectangle",
                   "12 current national flags carry the crescent moon for religious purposes",
                   "To celebrate the liberation from Turkish siege, Austrian bakers came up with a crescent-shaped pastry: the croissant",
                   "Flags with lions: Spain, Fiji, and Sri Lanka",
                   "Flags with the Southern Cross: Australia, New Zealand, Papua New Guinea, and Brazil",
-                  "The coat of arms in the Spanish flag is divided in 5 parts representing the territories of Castile, León, Aragon, Navarre, and Granada"];
+                  "The coat of arms in the Spanish flag 🇪🇸 is divided in 5 parts representing the territories of Castile, León, Aragon, Navarre, and Granada",
+                  "The flags of both Estonia 🇪🇪 and Ukraine 🇺🇦 are inspired by their landscapes"];
 output=facts_array[Math.floor(Math.random()*facts_array.length)]
