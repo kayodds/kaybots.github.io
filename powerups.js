dd6535a300d67b8b0ba23af3967bdd9a8d9646cc
kayodds <91171049+kayodds@users.noreply.github.com>
2022-04-03 16:26:22 -0300

    Create powerups.js

---
+++ powerups.js
@@ -0,0 +1,11 @@
+const powerup_array=["animal",
+                "archer","artist","backdrop","ball","balloon",
+                "beam","beetle","bell","bomb","bubble","burning",
+                "circus", "cleaning", "cook", "copy", "cupid",
+                "cutter", "doctor", "drill","fighter","fire",
+                "freeze", "ghost", "hammer","Hi-Jump", "ice",
+                "jet", "laser", "magic", "metal", "mini", "mirror",
+                "missile", "needle", "ninja", "parasol", "plasma",
+                "ranger", "sleep", "spark", "spear", "spider", "stone",
+                "sword", "throw","wheel","whip","wing","wrestler","yo-yo"];
+output=powerup_array[Math.floor(Math.random()*powerup_array.length)]
