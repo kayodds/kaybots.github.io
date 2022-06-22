b1545defd640b80e80aadcce9c4e65ed1cb4b2d9
kayodds <91171049+kayodds@users.noreply.github.com>
2022-06-22 15:22:02 -0300

    Create camdencracked.js

---
+++ camdencracked.js
@@ -0,0 +1,20 @@
+const cracked_array=["https://www.twitch.tv/italian6th/clip/ResoluteDaintyNoodleSuperVinlin-mgKq-m79EFG_-iSu",
+                     "https://www.twitch.tv/italian6th/clip/PlumpInquisitiveFennelNononoCat-MLWg5loAWPGAvuIB",
+                     "https://www.twitch.tv/italian6th/clip/PiercingAffluentStrawberryDatSheffy-ZNyglnNhhJ_T66F5",
+                     "https://www.twitch.tv/italian6th/clip/DelightfulDifficultMooseNerfBlueBlaster-p-abxuYg778N3ZWv",
+                     "https://www.twitch.tv/italian6th/clip/PatientStrongTrayHassaanChop",
+                     "https://www.twitch.tv/italian6th/clip/MotionlessCoyCattleDerp-1g5blRq54GawoduP",
+                     "https://www.twitch.tv/italian6th/clip/PuzzledLivelyWombatAllenHuhu-_xqK_h4zFnIcPJuq",
+                     "https://www.twitch.tv/italian6th/clip/FancySpinelessRaccoonM4xHeh-IkuJ6Yb84z8Db47c",
+                     "https://www.twitch.tv/italian6th/clip/DoubtfulFairWrenWutFace-AhhQ4e-bcuWk6-in",
+                     "https://www.twitch.tv/italian6th/clip/FreezingOutstandingShrimpTinyFace-aTxdlSEDd9UGLLxQ",
+                     "https://www.twitch.tv/italian6th/clip/AwkwardTallMeerkatDuDudu-cNFGszKcey2Z5QHm",
+                     "https://www.twitch.tv/italian6th/clip/OutstandingSpicyIcecreamMingLee-8sITCKMc_gyFPCRq",
+                     "https://www.twitch.tv/italian6th/clip/JollyLongCardYouDontSay",
+                     "https://www.twitch.tv/italian6th/clip/ColorfulEndearingSowPeoplesChamp-2ldqmQgD3_ncZHgu",
+                     "https://www.twitch.tv/italian6th/clip/InnocentKathishPuffinHassaanChop-D48znzs0SbGBjFPQ",
+                     "https://www.twitch.tv/italian6th/clip/EnchantingAcceptablePorpoiseRlyTho--O5rD0sNftH94Q_I",
+                     "https://www.twitch.tv/italian6th/clip/SavagePeacefulCurryMikeHogu-3XAIjATKYRzcuE_0",
+                     "https://www.twitch.tv/italian6th/clip/GeniusCarelessAsparagusRedCoat-IE0J9j8jHdJLYeDC"
+                   ];
+output=cracked_array[Math.floor(Math.random()*cracked_array.length)]
