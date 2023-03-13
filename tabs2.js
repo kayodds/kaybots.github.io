95f001274f0cea0f81f7baddfee2684e54d10e1a
kayodds <91171049+kayodds@users.noreply.github.com>
2023-03-12 21:22:10 -0300

    Create tabs2.js

---
+++ tabs2.js
@@ -0,0 +1,14 @@
+const tabs_array=["win in 5 seconds",
+                 "lose after one hit",
+                 "totally beat them",
+                 "decide it was a draw and go drink together",
+                 "be sent flying",
+                 "turn them into jelly",
+                 "be turned into dust",
+                 "suffer a terrible defeat",
+                 "become the hero of this land",
+                 "achieve victory after a lot of time",
+                 "be ran over in their way to the battle",
+                 "hit them with a bucket of water",
+                 "win big"];
+output=tabs_array[Math.floor(Math.random()*tabs_array.length)]
