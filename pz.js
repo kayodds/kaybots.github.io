f1dcb4fa4abf7cd4cd7081099cba377663c91827
kayodds <91171049+kayodds@users.noreply.github.com>
2023-07-16 17:31:50 -0300

    Update pz.js

--- pz.js
+++ pz.js
@@ -1,7 +1,7 @@
 const sauce_array=[["BBQ sauce","Garlic Rub","Olive Oil","Pesto","Red Sauce","Spicy Calabrian Chilli Red Sauce","White Sauce", "No Sauce"],
                    ["Assiago", "Dairy-Free Cheese", "Feta", "Gorgonzola", "Mozzarela", "Parmesan", "Ricotta", "No Cheese"],
                    ["Artichokes", "Basil", "Black Olives", "Broccoli", "Corn", "Roasted Garlic", "Chopped Garlic", "Green Bell Peppers", "Jalapeños", "Mama Lil's Sweet Hot Peppas", "Mushrooms", "Oregano", "Pineapple", "Red Onions", "Red Peppers", "Rosemary", "Salt & Pepper", "Spinach", "Tomatoes"],
-                   ["Anchovies", "Bacon", "Canadian Bacon", "Grilled Chicken", "Ground Beef", "Mild Sausage", "Pepperoni", "Salami", "Spicy Chicken Sausage"],
+                   ["Anchovies", "Bacon", "Canadian Bacon", "Grilled Chicken", "Ground Beef", "Mild Sausage", "Pepperoni", "Salami", "Spicy Chicken Sausage", "Left Beef", "No Meat"],
                    ["Balsamic Fig Glaze", "BBQ Swirl", "Hot Buffalo Sauce", "Mike's Hot Honey", "Pesto Drizzle", "Ranch", "Red Sauce Dollops", "Sri-rancha"]
 ];
 op=sauce_array
