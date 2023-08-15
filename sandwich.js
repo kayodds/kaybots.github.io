2a3b4e537a34bd6ee73c0448080b3a1f185ccd25
kayodds <91171049+kayodds@users.noreply.github.com>
2023-08-15 18:05:38 -0300

    Create sandwich.js

---
+++ sandwich.js
@@ -0,0 +1,4 @@
+const sandwich_array=[["Apple","Avocado", "Bacon", "Banana", "Basil", "Cheese", "Cherry Tomatoes", "Chorizo", "Cucumber", "Egg", "Fried Fillet", "Green Bell Pepper", "Ham", "Hamburger", "Herbed Sausage", "Jalapeño", "Kiwi", "Klawf Stick", "Lettuce", "Noodles", "Onion", "Pickle", "Pineapple", "Potato Salad", "Potato Tortilla", "Prosciutto", "Red Bell Pepper", "Red Onion", "Rice", "Smoked Fillet", "Strawberry", "Tofu", "Tomato", "Watercress", "Yellow Bell Pepper"],
+                      ["Butter", "Chili Sauce", "Cream Cheese", "Curry Powder", "Horseradish", "Jam", "Ketchup", "Marmalade", "Mayonnaise", "Mustard", "Olive Oil", "Peanut Butter", "Pepper", "Salt", "Vinegar", "Wasabi", "Whipped Cream", "Yogurt", "Bitter Herba Mystica", "Spicy Herba Mystica", "Salty Herba Mystica", "Sour Herba Mystica", "Sweet Herba Mystica"]
+];
+op=sandwich_array
